a8acbba6985d452a2d1e34f80be9faf2bfba2309
Alex Otoo <alexotoous@outlook.com>
2023-02-03 17:46:06 -0500

    http request exceptions

--- src/main/java/com/alexotoodev/resfulapiservice/user/UserResource.java
+++ src/main/java/com/alexotoodev/resfulapiservice/user/UserResource.java
@@ -1,12 +1,15 @@
 package com.alexotoodev.resfulapiservice.user;
 
+import java.net.URI;
 import java.util.List;
 
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 @RestController
 public class UserResource {
@@ -23,13 +26,30 @@ public class UserResource {
 
     //Get /users
     @GetMapping("/users/{id}")
-    public User getUserByID(@PathVariable int id) { return service.findOne(id);}
+    public User getUserByID(@PathVariable int id) {
+         User user = service.findOne(id);
+
+         if(user == null){
+            throw new UserNotFoundException("id:"+id);
+         }
+         return user;
+        }
 
     //Post /users
     @PostMapping("/users")
-    public void createUser(@RequestBody User user){
-        
-        
+    public ResponseEntity<User> createUser(@RequestBody User user){
+        User savedUser = service.save(user);
+       
+        //returns the location of the created user.
+        URI location = ServletUriComponentsBuilder.fromCurrentRequest()
+        .path("/{id}")
+        .buildAndExpand(savedUser
+        .getId()).toUri();
+
+        return ResponseEntity.created(location).build();
+
+
+
     }
 
 }
