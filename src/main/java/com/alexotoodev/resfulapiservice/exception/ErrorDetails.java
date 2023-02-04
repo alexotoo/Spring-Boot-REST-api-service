e0718b43eb7798681637f2becf677ae05ebf5d6b
Alex Otoo <alexotoous@outlook.com>
2023-02-04 17:17:52 -0500

    response error handler done

--- src/main/java/com/alexotoodev/resfulapiservice/exception/ErrorDetails.java
+++ src/main/java/com/alexotoodev/resfulapiservice/exception/ErrorDetails.java
@@ -1,19 +1,20 @@
 package com.alexotoodev.resfulapiservice.exception;
 
 import java.time.LocalDate;
+import java.time.LocalDateTime;
 
 public class ErrorDetails {
-    private LocalDate timestamp;
+    private LocalDateTime timestamp;
     private String message;
     private String details;
 
-    public ErrorDetails(LocalDate timestamp, String message, String details) {
+    public ErrorDetails(LocalDateTime timestamp, String message, String details) {
         this.timestamp = timestamp;
         this.message = message;
         this.details = details;
     }
 
-    public LocalDate getTimestamp() {
+    public LocalDateTime getTimestamp() {
         return timestamp;
     }
 
