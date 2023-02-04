64782417399a48fdc92d31ddb838843d77b282be
Alex Otoo <alexotoous@outlook.com>
2023-02-04 18:04:57 -0500

    delete user implemented

--- src/main/java/com/alexotoodev/resfulapiservice/user/UserDaoService.java
+++ src/main/java/com/alexotoodev/resfulapiservice/user/UserDaoService.java
@@ -34,5 +34,11 @@ public class UserDaoService {
         Predicate<? super User> predicate = user -> user.getId().equals(id);
         return users.stream().filter(predicate).findFirst().orElse(null);
     }
+
+
+    public void deleteById(int id) {
+        Predicate<? super User> predicate = user -> user.getId().equals(id);
+         users.removeIf(predicate);
+    }
     
 }
--- src/main/java/com/alexotoodev/resfulapiservice/user/UserResource.java
+++ src/main/java/com/alexotoodev/resfulapiservice/user/UserResource.java
@@ -4,6 +4,7 @@ import java.net.URI;
 import java.util.List;
 
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -34,6 +35,15 @@ public class UserResource {
          }
          return user;
         }
+
+
+    //Delete /user
+    @DeleteMapping("/users/{id}")
+    public void deleteUserByID(@PathVariable int id) {
+      service.deleteById(id);
+        }
+
+    
 
     //Post /users
     @PostMapping("/users")
